98f17da64a781b60748c18753a3922f3b1558a53
Henry Hu <henry.hu.sh@gmail.com>
2017-01-28 12:21:35 -0500

    indents

--- acpi_asus_als.c
+++ acpi_asus_als.c
@@ -64,8 +64,8 @@ static int	acpi_asus_als_probe(device_t dev);
 static int	acpi_asus_als_attach(device_t dev);
 static int	acpi_asus_als_detach(device_t dev);
 
-static int  acpi_asus_als_sysctl(SYSCTL_HANDLER_ARGS);
-static int  acpi_asus_als_sysctl_get(struct acpi_asus_als_softc *sc);
+static int	acpi_asus_als_sysctl(SYSCTL_HANDLER_ARGS);
+static int	acpi_asus_als_sysctl_get(struct acpi_asus_als_softc *sc);
 
 static device_method_t acpi_asus_als_methods[] = {
 	DEVMETHOD(device_probe,  acpi_asus_als_probe),
@@ -83,7 +83,8 @@ static driver_t acpi_asus_als_driver = {
 
 static devclass_t acpi_asus_als_devclass;
 
-DRIVER_MODULE(acpi_asus_als, acpi, acpi_asus_als_driver, acpi_asus_als_devclass, 0, 0);
+DRIVER_MODULE(acpi_asus_als, acpi, acpi_asus_als_driver,
+	acpi_asus_als_devclass, 0, 0);
 MODULE_DEPEND(acpi_asus_als, acpi, 1, 1, 1);
 
 static int
@@ -91,112 +92,112 @@ acpi_asus_als_probe(device_t dev)
 {
 	static char		*asus_ids[] = { "ACPI0008", NULL };
 
-    struct acpi_asus_als_softc	*sc;
-    char *rstr;
+	struct acpi_asus_als_softc	*sc;
+	char				*rstr;
 
 	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
-    if (acpi_disabled("asus_als"))
-        return (ENXIO);
+	if (acpi_disabled("asus_als"))
+		return (ENXIO);
 
-    /* Check if ambient light sensor is present. */
-    rstr = ACPI_ID_PROBE(device_get_parent(dev), dev, asus_ids);
-    if (rstr == NULL) {
-        return (ENXIO);
-    }
+	/* Check if ambient light sensor is present. */
+	rstr = ACPI_ID_PROBE(device_get_parent(dev), dev, asus_ids);
+	if (rstr == NULL) {
+		return (ENXIO);
+	}
 
-    sc = device_get_softc(dev);
-    sc->dev = dev;
-    sc->handle = acpi_get_handle(dev);
+	sc = device_get_softc(dev);
+	sc->dev = dev;
+	sc->handle = acpi_get_handle(dev);
 
-    device_set_desc_copy(dev, "ASUS ambient light sensor");
+	device_set_desc_copy(dev, "ASUS ambient light sensor");
 
-    return 0;
+	return 0;
 }
 
 static int
 acpi_asus_als_attach(device_t dev)
 {
-    struct acpi_asus_als_softc	*sc;
-    struct acpi_softc	*acpi_sc;
+	struct acpi_asus_als_softc	*sc;
+	struct acpi_softc		*acpi_sc;
 
-    ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
+	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
-    sc = device_get_softc(dev);
-    acpi_sc = acpi_device_get_parent_softc(dev);
+	sc = device_get_softc(dev);
+	acpi_sc = acpi_device_get_parent_softc(dev);
 
-    sysctl_ctx_init(&sc->sysctl_ctx);
-    sc->sysctl_tree = SYSCTL_ADD_NODE(&sc->sysctl_ctx,
-            SYSCTL_CHILDREN(acpi_sc->acpi_sysctl_tree),
-            OID_AUTO, "asus_als", CTLFLAG_RD, 0, "");
+	sysctl_ctx_init(&sc->sysctl_ctx);
+	sc->sysctl_tree = SYSCTL_ADD_NODE(&sc->sysctl_ctx,
+		SYSCTL_CHILDREN(acpi_sc->acpi_sysctl_tree),
+		OID_AUTO, "asus_als", CTLFLAG_RD, 0, "");
 
-    SYSCTL_ADD_PROC(&sc->sysctl_ctx,
-            SYSCTL_CHILDREN(sc->sysctl_tree), OID_AUTO,
-            "light", CTLTYPE_INT | CTLFLAG_RD | CTLFLAG_ANYBODY,
-            sc, 0, acpi_asus_als_sysctl, "I", "Ambient light value");
+	SYSCTL_ADD_PROC(&sc->sysctl_ctx,
+		SYSCTL_CHILDREN(sc->sysctl_tree), OID_AUTO,
+		"light", CTLTYPE_INT | CTLFLAG_RD | CTLFLAG_ANYBODY,
+		sc, 0, acpi_asus_als_sysctl, "I", "Ambient light value");
 
-    return (0);
+	return (0);
 }
 
 static int
 acpi_asus_als_detach(device_t dev)
 {
-    struct acpi_asus_als_softc	*sc;
+	struct acpi_asus_als_softc	*sc;
 
-    ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
+	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
-    sc = device_get_softc(dev);
+	sc = device_get_softc(dev);
 
-    sysctl_ctx_free(&sc->sysctl_ctx);
+	sysctl_ctx_free(&sc->sysctl_ctx);
 
-    return (0);
+	return (0);
 }
 
 static int
 acpi_asus_als_sysctl(SYSCTL_HANDLER_ARGS)
 {
-    struct acpi_asus_als_softc	*sc;
-    int value;
-    int error = 0;
+	struct acpi_asus_als_softc	*sc;
+	int				value;
+	int				error = 0;
 
 	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
-    sc = (struct acpi_asus_als_softc *)oidp->oid_arg1;
+	sc = (struct acpi_asus_als_softc *)oidp->oid_arg1;
 
 	ACPI_SERIAL_BEGIN(asus_als);
 
-    value = acpi_asus_als_sysctl_get(sc);
-    error = sysctl_handle_int(oidp, &value, 0, req);
+	value = acpi_asus_als_sysctl_get(sc);
+	error = sysctl_handle_int(oidp, &value, 0, req);
 
 	ACPI_SERIAL_END(asus_als);
 
-    return (error);
+	return (error);
 }
 
 static int
 acpi_asus_als_sysctl_get(struct acpi_asus_als_softc *sc)
 {
-    ACPI_BUFFER Buf;
-    ACPI_OBJECT *Obj;
-    int value = 0;
+	ACPI_BUFFER Buf;
+	ACPI_OBJECT *Obj;
+	int         value = 0;
 
-    ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
-    ACPI_SERIAL_ASSERT(asus_als);
+	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
+	ACPI_SERIAL_ASSERT(asus_als);
 
-    Buf.Pointer = NULL;
-    Buf.Length = ACPI_ALLOCATE_BUFFER;
+	Buf.Pointer = NULL;
+	Buf.Length = ACPI_ALLOCATE_BUFFER;
 
-    /* Calls _ALI to get the value. */
-    AcpiEvaluateObject(sc->handle, "_ALI", NULL, &Buf);
-    Obj = Buf.Pointer;
+	/* Calls _ALI to get the value. */
+	AcpiEvaluateObject(sc->handle, "_ALI", NULL, &Buf);
+	Obj = Buf.Pointer;
 
-    if (Obj->Type != ACPI_TYPE_INTEGER) {
-        device_printf(sc->dev, "_ALI returned non-integer\n");
-    } else {
-        value = Obj->Integer.Value;
-    }
+	if (Obj->Type != ACPI_TYPE_INTEGER) {
+		device_printf(sc->dev, "_ALI returned non-integer\n");
+	} else {
+		value = Obj->Integer.Value;
+	}
 
-    AcpiOsFree(Buf.Pointer);
+	AcpiOsFree(Buf.Pointer);
 
-    return (value);
+	return (value);
 }
